93ef40648c670095d3802bc7182fff978c6564f0
Yuliia-Oliinyk777 <yuliya2474@gmail.com>
2024-11-04 00:02:30 +0100

    add folders

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-// import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
 import 'modern-normalize';
 import './index.css';
-import App from './components/App/App';
-// import App from './App';
-// import PropTypes from "prop-types";
-// import clsx from "clsx";
+import App from './App';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
