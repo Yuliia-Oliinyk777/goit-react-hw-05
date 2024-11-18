d7cf1630fcd091e50b9103f7bd02340c00fc6317
Yuliia-Oliinyk777 <yuliya2474@gmail.com>
2024-11-18 17:37:51 +0100

    update config

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
-import Header from './components/Header/Header';
 import HomePage from './pages/HomePage/HomePage';
 import MoviesPage from './pages/MoviesPage/MoviesPage';
 import MovieDetailsPage from './pages/MovieDetailsPage/MovieDetailsPage';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 import MovieCast from './components/MovieCast/MovieCast';
 import MovieReviews from './components/MovieReviews/MovieReviews';
+import Navigation from './components/Navigation/Navigation';
 const App = () => {
   return (
     <div>
-      <Header />
+      <Navigation />
       <Routes>
         <Route path={'/'} element={<HomePage />} />
         <Route path={'/movies'} element={<MoviesPage />} />
